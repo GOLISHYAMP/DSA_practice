e2389aa01987c6ffa74d04b58b1f2a9f008d012d
Shyam Goli <golishyamgolisai@gmail.com>
2023-09-16 12:24:04 +0530

    sorting 1 completed

--- Sorting1/InsertionSort.py
+++ Sorting1/InsertionSort.py
@@ -2,11 +2,9 @@ n = 6
 arr = [2,13,1,52,20,9]
 # [2,13,1,52,20,9]
 
-for i in range(1,n):
-    k = i-1
-    # print("Value of k: ",k)
-    while (arr[k] > arr[k+1]) and k>=0:
-        arr[k],arr[k+1] = arr[k+1],arr[k]
+for i in range(n):
+    k = i
+    while(arr[k]<arr[k-1] and k != 0):
+        arr[k],arr[k-1] = arr[k-1],arr[k]
         k -= 1
-    print(arr)
 print(arr)
