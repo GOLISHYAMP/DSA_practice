7db23968309e206a851bc7b4a0317d049dba2a3c
Shyam Goli <golishyamgolisai@gmail.com>
2023-09-19 12:30:03 +0530

    I tried to rewrite the sortting algo

--- Sorting1/BubbleSort.py
+++ Sorting1/BubbleSort.py
@@ -6,3 +6,11 @@ for i in reversed(range(n-1)):
         if arr[j]>arr[j+1]:
             arr[j],arr[j+1] = arr[j+1],arr[j]
 print(arr)
+
+# for i in range(n-2,-1,-1):
+#     k = 0
+#     while(k <= i):
+#         if arr[k] > arr[k+1]:
+#             arr[k], arr[k+1] = arr[k+1],arr[k]
+#         k += 1
+# print(arr)
--- Sorting1/InsertionSort.py
+++ Sorting1/InsertionSort.py
@@ -8,3 +8,11 @@ for i in range(n):
         arr[k],arr[k-1] = arr[k-1],arr[k]
         k -= 1
 print(arr)
+
+# for i in range(1,n):
+#     # print("i = ",i)
+#     for k in range(i,0,-1):
+#         if arr[k]<arr[k-1]:
+#             arr[k],arr[k-1] = arr[k-1],arr[k]
+#         # print(k)
+# print(arr)
